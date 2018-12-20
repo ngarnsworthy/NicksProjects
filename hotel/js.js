7672ce180bf2718c7ecec8358b55c7dda2de8fe7
ngarnsworthy <ngarnsworthy@gmail.com>
2018-12-20 16:44:37 -0600

    idimis

--- hotel/js.js
+++ hotel/js.js
@@ -1,11 +1,18 @@
 var days = 0;
 var other = 0;
 var sum = 0;
-
+var coffee = 0;
+var heated_sheats = 0;
+var trip = 0;
 function reseat() {
   do_calc();
-  $(idDays).html(sum);
-  $(idOther).html(other);
+    $(idDays).html(days*50);
+    $(idHeated_sheats).html(heated_sheats);
+    $(idTrip).html(trip);
+    $(idCoffee).html(coffee);
+    $(idTotal).html(sum);
+
+
 
 
   window.print();
@@ -15,15 +22,17 @@ function do_calc() {
   days = document.getElementById('days').value - 0;
   // other = document.getElementById('other').value - 0;
   if($(idHeated).prop('checked')) {
-    other = ($(heated_sheatsA).val()-0) * 20;
+    heated_sheats = other = ($(heated_sheatsA).val()-0) * 20 ;
   }
 
   if($(idTrip).prop('checked')) {
-    other += ($(tripA).val()-0) * 100;
+    trip = ($(tripA).val()-0) * 100;
+    other += trip;
   }
 
   if($(idCoffee).prop('checked')) {
-    other += ($(coffeeA).val()-0)*3;
+    coffee = ($(coffeeA).val()-0)*3
+    other += coffee;
   }
 
   sum = (days*50)+other;
